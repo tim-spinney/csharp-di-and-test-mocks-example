ecc1264abd2e1dc2d37b9559003a89ce5a6921a3
Tim Spinney <tspinney@seattleu.edu>
2025-05-30 12:42:19 -0700

    added unit test stubs

---
+++ AutomatedTests/DatabaseTest.cs
@@ -0,0 +1,33 @@
+﻿using DependencyInjection;
+
+namespace AutomatedTests;
+
+[TestClass]
+public class DatabaseTest
+{
+    [TestMethod("Purchasing a product decreases its quantity")]
+    public void PurchasingProductDecreasesQuantity()
+    {
+        // Arrange
+        Database database = new Database();
+        
+        // Act
+        database.PurchaseProduct(1, 1);
+        
+        // Assert
+        database.FindInStock();
+        // Wait... we can't see the output it wrote to the console
+    }
+
+    [TestMethod("Trying to purchase more of a product than is in stock fails")]
+    public void PurchasingTooManyFails()
+    {
+        // Arrange
+        Database database = new Database();
+        
+        // Act
+        database.PurchaseProduct(1, 10);
+        
+        // Uhh... how do we assert?
+    }
+}
